0e9717d1448c9c9cb7bdfd17a44b484ba6f983f4
Tester-1 <testing-1@users.noreply.github.com>
2015-02-16 19:47:55 -0800

    Make book index list books from DB

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def index
-	@available_at = Time.now
-	@books = ["Atlas Shrugged", "Ender's Game", "Hamlet", "The Hobbit"]
+    @available_at = Time.now
+    @books = Book.all
   end
 end
