91ebadcae3783ccfc3ccd4407d6d8a211b6e987c
Tester-1 <testing-1@users.noreply.github.com>
2015-02-20 11:46:02 -0800

    Add nested review routes. Add ability to view book reviews.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :reviews
-
-  root 'books#index'
-
   resources :books do
-      get 'page/:page', :action => :index, :on => :collection
+    resources :reviews
+    get 'page/:page', :action => :index, :on => :collection
   end
+  root 'books#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
