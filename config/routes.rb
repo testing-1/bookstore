d2678f84ed8726e89c41a65193f5918deb8b1339
Tester-1 <testing-1@users.noreply.github.com>
2015-02-19 18:03:42 -0800

    Add layouts and a root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'books#index'
+
   # get "/books" => "books#index", as: 'books'
   # get "/books/new" => "books#new", as: 'new_book'
   # get "/books/:id" => "books#show", as: 'book'
